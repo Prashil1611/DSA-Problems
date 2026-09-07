549a84360700449a19a42b1d8a9ce8e27d15a359
Prashil Kamble <prashilkamble300@gmail.com>
2026-09-08 01:59:55 +0530

    Time: 352 ms (5.66%), Space: 47.8 MB (62.39%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, -1);
+
+        return helper(nums, 0, dp);
+        
+    }
+
+    private boolean helper(int[] nums, int i, int[] dp){
+
+        if(i >= nums.length - 1) return true;
+
+        if(dp[i] != -1){
+            return dp[i] == 1;
+        }
+
+        for (int jump = 1; jump <= nums[i]; jump++){
+
+            if(helper(nums, jump + i, dp)){
+                dp[i] = 1;
+                return true;
+            }
+        }
+
+        dp[i] = 0;
+
+        return false;
+    }
+}
