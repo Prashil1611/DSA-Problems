259ccd622436155b2a3d29bfd9ce2e7cb3318c50
Prashil Kamble <prashilkamble300@gmail.com>
2026-09-25 19:51:01 +0530

    Time: 2 ms (89.62%), Space: 47.8 MB (80.18%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,31 +1,48 @@
 class Solution {
     public boolean canJump(int[] nums) {
 
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp, -1);
+        // int[] dp = new int[nums.length];
+        // Arrays.fill(dp, -1);
 
-        return helper(nums, 0, dp);
+        // return helper(nums, 0, dp);
+
+
+        int maxReach = 0;
+
+        for (int i = 0; i < nums.length; i++){
+            if(i > maxReach) return false;
+            maxReach = Math.max(maxReach, i+nums[i]);
+        }
+
+        return true;
+
+
         
     }
 
-    private boolean helper(int[] nums, int i, int[] dp){
+    // private boolean helper(int[] nums, int i, int[] dp){
 
-        if(i >= nums.length - 1) return true;
+    //     if(i >= nums.length - 1) return true;
 
-        if(dp[i] != -1){
-            return dp[i] == 1;
-        }
+    //     if(dp[i] != -1){
+    //         return dp[i] == 1;
+    //     }
 
-        for (int jump = 1; jump <= nums[i]; jump++){
+    //     for (int jump = 1; jump <= nums[i]; jump++){
 
-            if(helper(nums, jump + i, dp)){
-                dp[i] = 1;
-                return true;
-            }
-        }
+    //         if(helper(nums, jump + i, dp)){
+    //             dp[i] = 1;
+    //             return true;
+    //         }
+    //     }
 
-        dp[i] = 0;
+    //     dp[i] = 0;
 
-        return false;
-    }
+    //     return false;
+    // }
+
 }
+
+
+
+
